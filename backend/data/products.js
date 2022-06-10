82b3c3c1f16e22363385a6bb66a474e823cc428c
Glendon Chin <glendonchin@gmail.com>
2022-06-10 16:54:43 -0400

    update project!

---
+++ backend/data/products.js
@@ -0,0 +1,41 @@
+const products = [
+    {
+      name: 'Airpods Wireless Bluetooth Headphones',
+      image: '/images/airpods.jpg',
+      description:
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+      brand: 'Apple',
+      category: 'Electronics',
+      price: 89.99,
+      countInStock: 3,
+      rating: 0,
+      numReviews: 0,
+    },
+    {
+      name: 'iPhone 11 Pro',
+      image: '/images/phone.jpg',
+      description:
+        'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      brand: 'Apple',
+      category: 'Electronics',
+      price: 599.99,
+      countInStock: 10,
+      rating: 0,
+      numReviews: 0,
+    },
+    {
+      name: 'Cannon EOS 80D DSLR Camera',
+      image: '/images/camera.jpg',
+      description:
+        'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+      brand: 'Cannon',
+      category: 'Electronics',
+      price: 929.99,
+      countInStock: 0,
+      rating: 0,
+      numReviews: 0,
+    },
+  ]
+  
+  export default products
+  
